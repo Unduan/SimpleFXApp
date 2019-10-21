d24e06a8295348e228ef22dac9d6d54cb1cc9d91
Adrian Pelka <adrian.pelka@hotmail.com>
2019-10-21 20:38:12 +0200

    language properties and new fonts.

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,18 +1,42 @@
 package controllers;
 
+import configuration.LangProperties;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
     @FXML
     public Pane paneNewGame, paneLoadGame, paneScoreBoard, paneOptions;
 
     @FXML
+    private Button btnLoadGame, btnNewGame, btnOptions, btnScoreBoard, btnExitGame;
+
+    @FXML
     private AnchorPane mainScreenPanel;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        if(!LangProperties.TEXT_FILLER.isEmpty()){
+            btnNewGame.setText(LangProperties.TEXT_FILLER.getProperty("btnNewGame"));
+            btnLoadGame.setText(LangProperties.TEXT_FILLER.getProperty("btnLoadGame"));
+            btnOptions.setText(LangProperties.TEXT_FILLER.getProperty("btnOptions"));
+            btnScoreBoard.setText(LangProperties.TEXT_FILLER.getProperty("btnScoreBoard"));
+            btnExitGame.setText(LangProperties.TEXT_FILLER.getProperty("btnExitGame"));
+        }
+        mainScreenPanel.requestFocus();
+    }
 
     @FXML
     private void removeFocusFromButton(ActionEvent actionEvent) {
