ee171887561b848d814c71cf52e5f2d38991504a
Przemysław Bus <przgra69@gmail.com>
2019-10-21 19:50:08 +0200

    added main menu panels

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class MainController {
+    @FXML
+    public Pane paneNewGame, paneLoadGame, paneScoreBoard, paneOptions;
 
     @FXML
     private AnchorPane mainScreenPanel;
@@ -26,5 +30,41 @@ public class MainController {
 
     @FXML
     private void releasedOnButton(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void goToNewGamePanel(MouseEvent mouseEvent) {
+        paneNewGame.setVisible(true);
+        paneLoadGame.setVisible(false);
+        paneOptions.setVisible(false);
+        paneScoreBoard.setVisible(false);
+
+    }
+
+    public void goToLoadPanel(MouseEvent mouseEvent) {
+        paneNewGame.setVisible(false);
+        paneLoadGame.setVisible(true);
+        paneOptions.setVisible(false);
+        paneScoreBoard.setVisible(false);
+    }
+
+    public void goToOptions(MouseEvent mouseEvent) {
+        paneNewGame.setVisible(false);
+        paneLoadGame.setVisible(false);
+        paneOptions.setVisible(true);
+        paneScoreBoard.setVisible(false);
+    }
+
+    public void goToScoreBoard(MouseEvent mouseEvent) {
+        paneNewGame.setVisible(false);
+        paneLoadGame.setVisible(false);
+        paneOptions.setVisible(false);
+        paneScoreBoard.setVisible(true);
+    }
+
+    public void goToExitGame(MouseEvent mouseEvent) {
+        Platform.exit();
+
+
     }
 }
